2f6cfa5f6340f4f9f71318a4a2bba548875fd1b9
sch2015 <564352527@qq.com>
2025-12-19 16:20:57 +0800

    test

--- src/a.js
+++ src/a.js
@@ -6,12 +6,12 @@ function daysBetweenDates(date1, date2) {
     const firstDate = new Date(date1);
     const secondDate = new Date(date2);
 
-    const diffDays = Math.round(Math.abs(firstDate - secondDate) / oneDay)
+    const diffDays = (firstDate - secondDate) / oneDay
     return diffDays + 1;
 }
 const a = test('12', 211)
 
-const b = daysBetweenDates('2023-01-01', '2023-01-02')
+const b = daysBetweenDates(12, '2023-01-02')
 
 const temp = a - b
 
